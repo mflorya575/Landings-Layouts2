d886ef0aa1a8d28210b1b36bd2d0112753637ef7
Michael Florya <mih2303@mail.ru>
2024-06-09 17:37:18 +0300

    change some js code & change menu in some pages

--- 2_school_layout/js/costAddDisciplines.js
+++ 2_school_layout/js/costAddDisciplines.js
@@ -9,12 +9,15 @@ const costAddDisciplines = () => {
     costBtn.addEventListener('click', () => {
         const li = document.createElement('li');
         li.innerHTML = `
-        <div><input type="text" placeholder="Название предмета"></вшм>
+        <form class="box-item">
+                <div><input type="text" placeholder="Название предмета"></div>
         <div><input type="text" placeholder="Уровень (пример: 8 класс)"></div>
         <div><input type="text" placeholder="Индивидуально / в группе"></div>
         <div><input type="text" placeholder="Цена за академический час (45 мин)"></div>
         <div><input type="text" placeholder="Количество академических часов в месяц"></div>
         <div><input type="text" placeholder="Стоимость за месяц"></div>
+        </form>
+        <div class="box"><button>Сохранить</button><button>Редактировать</button><button>Удалить</button></div>
       `;
 
         const listItems = costList.querySelectorAll('li');
