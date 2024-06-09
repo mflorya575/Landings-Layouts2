b0bcb94887f0e30e45f250942021f285459865ee
Michael Florya <mih2303@mail.ru>
2024-06-09 16:57:44 +0300

    add some changes in js code & html

--- 2_school_layout/js/costAddDisciplines.js
+++ 2_school_layout/js/costAddDisciplines.js
@@ -9,12 +9,12 @@ const costAddDisciplines = () => {
     costBtn.addEventListener('click', () => {
         const li = document.createElement('li');
         li.innerHTML = `
-        <div><input type="text" placeholder="Название предмета"></div>
-        <div><input type="text" placeholder="Уровень (пример: 8 класс)"></div>
-        <div><input type="text" placeholder="Индивидуально / в группе"></div>
-        <div><input type="text" placeholder="Цена за академический час (45 мин)"></div>
-        <div><input type="text" placeholder="Количество академических часов в месяц"></div>
-        <div><input type="text" placeholder="Стоимость за месяц"></div>
+        <form><input type="text" placeholder="Название предмета"></form>
+        <form><input type="text" placeholder="Уровень (пример: 8 класс)"></form>
+        <form><input type="text" placeholder="Индивидуально / в группе"></form>
+        <form><input type="text" placeholder="Цена за академический час (45 мин)"></form>
+        <form><input type="text" placeholder="Количество академических часов в месяц"></form>
+        <form><input type="text" placeholder="Стоимость за месяц"></form>
       `;
 
         const listItems = costList.querySelectorAll('li');
